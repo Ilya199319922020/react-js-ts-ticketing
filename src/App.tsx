f70ec73f3551621f4c72f1946d30bbb5f4206df8
Ilya Smirnov <iliasmirnov93@mail.ru>
2022-08-05 20:02:34 +0300

    added style ticker

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import TicketsList from './components/TicketsList';
 
 const App: React.FC = () => {
--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -13,6 +13,17 @@ const Ticket: React.FC<TickProps> = ({ item }) => {
 		arrival_time, destination, destination_name, arrival_date, stops
 	} = item
 
+	const convertDate = (date: any) => date
+		.toLocaleDateString("ru", { day: "numeric", year: "numeric", weekday: 'short', month: "short" })
+		.replace(/[\s,. ]+/g, " ");
+
+	const currentDate = convertDate(new Date(departure_date)).split(' ')
+	const dateA = [currentDate[1], currentDate[2], currentDate[3]].join(' ')
+
+	const nextDate = convertDate(new Date(arrival_date)).split(' ')
+	const dateB = [currentDate[1], currentDate[2], currentDate[3]].join(' ')
+
+
 	return (<>
 		<div
 			className={style.ticket}
@@ -24,7 +35,18 @@ const Ticket: React.FC<TickProps> = ({ item }) => {
 					src={turkishairlines}
 					className={style.ticket__button_image}
 				/>
-				<button>Купить за {price} Р</button>
+				<button
+					className={style.ticket__btn}
+				>
+					<div>Купить</div>
+					<div
+						className={style.ticket__price}
+					>
+						за {price} <span>
+							P
+						</span>
+					</div>
+				</button>
 			</div>
 			<div
 				className={style.ticket__container}
@@ -32,14 +54,32 @@ const Ticket: React.FC<TickProps> = ({ item }) => {
 				<div
 					className={style.ticket__container_info}
 				>
-					<div>{departure_time}</div>
-					<div>{origin},{origin_name} </div>
-					<div>{departure_date}</div>
+					<div
+						className={style.ticket__container_time}
+					>
+						{departure_time}
+					</div>
+					<div
+						className={style.ticket__container_name}
+					>
+						{origin}, <span>
+							{origin_name}
+						</span>
+					</div>
+					<div
+						className={style.ticket__container_data}
+					>
+						<span>
+							{dateA}, {currentDate[0]}
+						</span>
+					</div>
 				</div>
 				<div
 					className={style.ticket__container_transfer}
 				>
-					<div>
+					<div
+						className={style.transfer__element_stops}
+					>
 						{stops} {
 							stops > 1
 								? 'ПЕРЕСАДКИ'
@@ -58,10 +98,26 @@ const Ticket: React.FC<TickProps> = ({ item }) => {
 						<img src={vector} />
 					</div>
 				</div>
-				<div>
-					<div>{arrival_time}</div>
-					<div>{destination_name},{destination} </div>
-					<div>{arrival_date}</div>
+				<div
+					className={style.ticket__container_info}
+				>
+					<div
+						className={style.ticket__container_time}
+					>
+						{arrival_time}
+					</div>
+					<div
+						className={style.ticket__container_name}
+					>
+						{destination_name}, <span>
+							{destination}
+						</span>
+					</div>
+					<div
+						className={style.ticket__container_data}
+					>
+						<span>{dateB}, {nextDate[0]}</span>
+					</div>
 				</div>
 			</div>
 		</div>
